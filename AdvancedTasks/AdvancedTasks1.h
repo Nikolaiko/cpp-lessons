f3eb6931910dc1910fee63688aa2510e058dca80
Nikolai <nbaklanov@issart.com>
2021-02-27 21:38:42 +0600

    c++ tasks

---
+++ AdvancedTasks/AdvancedTasks1.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+namespace Tasks {
+	bool isHeGonnaSurvive(int bullets, int dragons);					
+	string reversedStrings(string source);
+	int returnDubElement(int source[]);	
+	bool allianceHelp(int soldiersHealth[], int soldiersAttach[], int monsterHealth, int monsterAttack);
+};
+
---
+++ AdvancedTasks/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "AdvancedTasks1.h"
+
+using namespace std;
+using namespace Tasks;
+
+int main(int argc, char** argv) {
+	
+
+	
+	return 0;
+}
